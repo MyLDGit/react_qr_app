e6b3f941c0031bda87311bb4398affbde31ae27b
MyLDGit <mpohl@launchDarkly.com>
2025-02-10 16:15:35 -0700

    first update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { deviceType, osName } from "react-device-detect";
 import getUserId from "./util/getUserId";
 
-const CLIENTKEY = "609ead905193530d7c28647b";
+const CLIENTKEY = "67a6890859e02309ba5f44d5";
+
 
 let id = getUserId();
 
